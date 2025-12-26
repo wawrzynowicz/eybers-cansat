ef819a4b1a7436f02ae06f558dcc7498c5961857
EYBERS <eybers.cansat@gmail.com>
2025-12-26 15:42:10 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,11 +1,9 @@
 import Home from './pages/Home';
-import MuonInfo from './pages/MuonInfo';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Home": Home,
-    "MuonInfo": MuonInfo,
 }
 
 export const pagesConfig = {
